e40b6f792e96a76c29a6c6fbee6d3374c5c602ff
Nagasai Krishna <126434821+nskkkkk@users.noreply.github.com>
2023-09-02 22:47:14 -0400

    Add files via upload

---
+++ swenhw1/src/Carroll.java
@@ -0,0 +1,20 @@
+/* carroll
+ Naga Sai Krishna Kesani
+ nk4689@rit.edu
+*/
+public class Carroll
+{
+    /* The main class will print the statement given in the image
+    */
+    public static void main(String[] args)
+    {
+        System.out.println("\"Begin at the");
+        System.out.println("beginning,\", the King");
+        System.out.println("said, very gravely,\"and");
+        System.out.println("go on till you come to ");
+        System.out.println("the end: then stop.\" ");
+        System.out.println("         -Lewis Carroll");
+    }
+
+}
+// End of class Carroll
